2c4cd4247142bcfbce2ca290c92ea525ccc15c12
yuta0306 <you-2001-3-6@ezweb.ne.jp>
2022-07-13 13:00:16 +0900

    checkpoint

--- faster_transformer/models/__init__.py
+++ faster_transformer/models/__init__.py
@@ -1,5 +1,17 @@
 from .attention import FasterMultiHeadAttention
+from .transformer import (
+    FasterTransformer,
+    FasterTransformerDecoder,
+    FasterTransformerDecoderLayer,
+    FasterTransformerEncoder,
+    FasterTransformerEncoderLayer,
+)
 
 __all__ = [
     "FasterMultiHeadAttention",
+    "FasterTransformer",
+    "FasterTransformerDecoder",
+    "FasterTransformerDecoderLayer",
+    "FasterTransformerEncoder",
+    "FasterTransformerEncoderLayer",
 ]
--- tests/test_faster_transformer.py
+++ tests/test_faster_transformer.py
@@ -1,6 +1,10 @@
+from collections import OrderedDict
+
+import pytest
 import torch
 import torch.nn as nn
-from faster_transformer import FasterMultiHeadAttention, __version__
+from faster_transformer import __version__
+from faster_transformer.models import FasterMultiHeadAttention, FasterTransformer
 
 
 def build_models():
@@ -32,6 +36,7 @@ def test_version():
     assert __version__ == "0.1.0"
 
 
+@pytest.mark.skip()
 def test_attention():
     builtin, model = build_models()
 
@@ -49,6 +54,7 @@ def test_attention():
     assert nn.MSELoss()(output, output_builtin) < eps
 
 
+@pytest.mark.skip()
 def test_attention_with_mask():
     builtin, model = build_models()
 
@@ -65,3 +71,34 @@ def test_attention_with_mask():
 
     eps = 1e-6
     assert nn.MSELoss()(output, output_builtin) < eps
+
+
+def test_transformer():
+    builtin = nn.Transformer()
+    state_dict = builtin.state_dict()
+    state_dict_new = OrderedDict()
+    for k, v in state_dict.items():
+        if "in_proj_weight" in k:
+            base = k.replace(".in_proj_weight", "")
+            Wq, Wk, Wv = v.chunk(3, 0)
+            state_dict_new[f"{base}.q_proj.weight"] = Wq
+            state_dict_new[f"{base}.k_proj.weight"] = Wk
+            state_dict_new[f"{base}.v_proj.weight"] = Wv
+        elif "in_proj_bias" in k:
+            base = k.replace(".in_proj_bias", "")
+            bias_q, bias_k, bias_v = v.chunk(3, 0)
+            state_dict_new[f"{base}.q_proj.bais"] = bias_q
+            state_dict_new[f"{base}.k_proj.bais"] = bias_k
+            state_dict_new[f"{base}.v_proj.bais"] = bias_v
+        else:
+            state_dict_new[k] = v
+    model = FasterTransformer(activation="relu")
+    keys = [n for n, p in model.named_parameters()]
+    for key in state_dict_new.keys():
+        if key not in keys:
+            print(key)
+    print()
+    for key in keys:
+        if key not in state_dict_new.keys():
+            print(key)
+    model.load_state_dict(state_dict_new, strict=True)
