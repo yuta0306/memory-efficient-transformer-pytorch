43f9073464a15e74623d1d87e06e09be825ce3c0
yuta0306 <you-2001-3-6@ezweb.ne.jp>
2022-07-13 12:22:15 +0900

    add decoder & transformer

--- faster_transformer/models/transformer.py
+++ faster_transformer/models/transformer.py
@@ -238,3 +238,210 @@ class FasterTransformerEncoderLayer(nn.Module):
     def _ff_block(self, x: torch.Tensor) -> torch.Tensor:
         x = self.linear2(self.dropout(self.activation(self.linear1(x))))
         return self.dropout2(x)
+
+
+class FasterTransformerDecoder(nn.Module):
+
+    __constants__ = ["norm"]
+
+    def __init__(
+        self,
+        decoder_layer: nn.Module,
+        num_layers: int,
+        norm: Optional[nn.Module] = None,
+    ) -> None:
+        """
+        Parameters
+        ----------
+            decoder_layer: an instance of the TransformerDecoderLayer() class (required).
+            num_layers: the number of sub-decoder-layers in the decoder (required).
+            norm: the layer normalization component (optional).
+
+        """
+        super(FasterTransformerDecoder, self).__init__()
+        self.layers = nn.ModuleList(
+            [copy.deepcopy(decoder_layer) for _ in range(num_layers)]
+        )
+        self.num_layers = num_layers
+        self.norm = norm
+
+    def forward(
+        self,
+        tgt: torch.Tensor,
+        memory: torch.Tensor,
+        tgt_mask: Optional[torch.Tensor] = None,
+        memory_mask: Optional[torch.Tensor] = None,
+        tgt_key_padding_mask: Optional[torch.Tensor] = None,
+        memory_key_padding_mask: Optional[torch.Tensor] = None,
+    ) -> torch.Tensor:
+        """
+        Parameters
+        ----------
+        tgt
+            the sequence to the decoder (required).
+        memory
+            the sequence from the last layer of the encoder (required).
+        tgt_mask
+            the mask for the tgt sequence (optional).
+        memory_mask
+            the mask for the memory sequence (optional).
+        tgt_key_padding_mask
+            the mask for the tgt keys per batch (optional).
+        memory_key_padding_mask
+            The mask for the memory keys per batch (optional).
+        """
+        output = tgt
+
+        for mod in self.layers:
+            output = mod(
+                output,
+                memory,
+                tgt_mask=tgt_mask,
+                memory_mask=memory_mask,
+                tgt_key_padding_mask=tgt_key_padding_mask,
+                memory_key_padding_mask=memory_key_padding_mask,
+            )
+
+        if self.norm is not None:
+            output = self.norm(output)
+
+        return output
+
+
+class FasterTransformerDecoderLayer(nn.Module):
+    __constants__ = ["batch_first", "norm_first"]
+
+    def __init__(
+        self,
+        d_model: int,
+        nhead: int,
+        dim_feedforward: int = 2048,
+        dropout: float = 0.1,
+        activation: Literal["relu", "gelu"] = "gelu",
+        layer_norm_eps: float = 1e-5,
+        pre_norm: bool = False,
+        device=None,
+        dtype=None,
+    ) -> None:
+        factory_kwargs = {"device": device, "dtype": dtype}
+        super(FasterTransformerDecoderLayer, self).__init__()
+        self.self_attn = FasterMultiHeadAttention(
+            embed_dim=d_model,
+            num_heads=nhead,
+            dropout=dropout,
+            batch_first=True,
+            device=device,
+            dtype=dtype,
+            return_attention_weights=False,
+        )
+        self.multihead_attn = FasterMultiHeadAttention(
+            embed_dim=d_model,
+            num_heads=nhead,
+            dropout=dropout,
+            batch_first=True,
+            device=device,
+            dtype=dtype,
+            return_attention_weights=False,
+        )
+        # Implementation of Feedforward model
+        self.linear1 = nn.Linear(d_model, dim_feedforward, **factory_kwargs)
+        self.dropout = nn.Dropout(dropout)
+        self.linear2 = nn.Linear(dim_feedforward, d_model, **factory_kwargs)
+
+        self.pre_norm = pre_norm
+        self.norm1 = nn.LayerNorm(d_model, eps=layer_norm_eps, **factory_kwargs)
+        self.norm2 = nn.LayerNorm(d_model, eps=layer_norm_eps, **factory_kwargs)
+        self.norm3 = nn.LayerNorm(d_model, eps=layer_norm_eps, **factory_kwargs)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+        self.dropout3 = nn.Dropout(dropout)
+
+        # Legacy string support for activation function.
+        if activation == "gelu":
+            self.activation: nn.Module = nn.GELU()
+        elif activation == "relu":
+            self.activation = nn.ReLU()
+
+    def __setstate__(self, state):
+        if "activation" not in state:
+            state["activation"] = nn.ReLU()
+        super(FasterTransformerDecoderLayer, self).__setstate__(state)
+
+    def forward(
+        self,
+        tgt: torch.Tensor,
+        memory: torch.Tensor,
+        tgt_mask: Optional[torch.Tensor] = None,
+        memory_mask: Optional[torch.Tensor] = None,
+        tgt_key_padding_mask: Optional[torch.Tensor] = None,
+        memory_key_padding_mask: Optional[torch.Tensor] = None,
+    ) -> torch.Tensor:
+        """
+        Parameters
+        ----------
+        tgt
+            the sequence to the decoder layer (required).
+        memory
+            the sequence from the last layer of the encoder (required).
+        tgt_mask
+            the mask for the tgt sequence (optional).
+        memory_mask
+            the mask for the memory sequence (optional).
+        tgt_key_padding_mask
+            the mask for the tgt keys per batch (optional).
+        memory_key_padding_mask
+            the mask for the memory keys per batch (optional).
+        """
+        x = tgt
+        if self.pre_norm:
+            x = x + self._sa_block(self.norm1(x), tgt_mask, tgt_key_padding_mask)
+            x = x + self._mha_block(
+                self.norm2(x), memory, memory_mask, memory_key_padding_mask
+            )
+            x = x + self._ff_block(self.norm3(x))
+        else:
+            x = self.norm1(x + self._sa_block(x, tgt_mask, tgt_key_padding_mask))
+            x = self.norm2(
+                x + self._mha_block(x, memory, memory_mask, memory_key_padding_mask)
+            )
+            x = self.norm3(x + self._ff_block(x))
+
+        return x
+
+    # self-attention block
+    def _sa_block(
+        self,
+        x: torch.Tensor,
+        attn_mask: Optional[torch.Tensor],
+        key_padding_mask: Optional[torch.Tensor],
+    ) -> torch.Tensor:
+        x = self.self_attn(
+            x,
+            x,
+            x,
+            attn_mask=attn_mask,
+            key_padding_mask=key_padding_mask,
+        )
+        return self.dropout1(x)
+
+    # multihead attention block
+    def _mha_block(
+        self,
+        x: torch.Tensor,
+        mem: torch.Tensor,
+        attn_mask: Optional[torch.Tensor],
+        key_padding_mask: Optional[torch.Tensor],
+    ) -> torch.Tensor:
+        x = self.multihead_attn(
+            x,
+            mem,
+            mem,
+            attn_mask=attn_mask,
+            key_padding_mask=key_padding_mask,
+        )
+        return self.dropout2(x)
+
+    # feed forward block
+    def _ff_block(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.linear2(self.dropout(self.activation(self.linear1(x))))
+        return self.dropout3(x)
